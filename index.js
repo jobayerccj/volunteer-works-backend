0170963f84f8c22ad3b382e0ba00230b586422d3
Al Md. Jobayer Islam <jobayer.it@generalpharma.com>
2020-10-05 12:05:31 +0600

    add methods added

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 app.use(cors());
 app.use(bodyParser.json());
+const ObjectId = require('mongodb').ObjectId;
 
 const dotenv = require('dotenv');
 dotenv.config();
@@ -20,6 +21,7 @@ const uri = "mongodb+srv://jobayer:"+process.env.DBPASSWORD+"@cluster0.wlizm.mon
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
     const events = client.db("volunteer-works").collection("events");
+    const registeredEvents = client.db("volunteer-works").collection("registeredUsers");
 
     app.get('/events', (req, res) => {
         events.find({})
@@ -27,7 +29,49 @@ client.connect(err => {
             console.log(err);
             res.status(200).send(documents);
         });
-    })
+    });
+
+    app.post('/addEventRegistration', (req, res)=>{
+
+        const newRegistration = req.body;
+        registeredEvents.insertOne(newRegistration)
+            .then(result => {
+                res.send(result.insertedCount > 0);
+            })
+    });
+
+    app.get('/registeredevents', (req, res) => {
+        //console.log(req.header('email'));
+        registeredEvents.find({ email: req.header('email')})
+            .toArray((err, documents) => {
+                res.status(200).send(documents);
+            });
+    });
+
+    app.delete('/deleteregistration/:id', (req, res) =>{
+        registeredEvents.deleteOne({_id: ObjectId(req.params.id)})
+            .then( result => {
+                res.send(result.deletedCount > 0);
+            })
+    });
+
+    app.get('/admin', (req, res) => {
+        //console.log(req.header('email'));
+        registeredEvents.find({})
+            .toArray((err, documents) => {
+                res.status(200).send(documents);
+            });
+    });
+
+    app.post('/addEvent', (req, res)=>{
+
+        const newEvent = req.body;
+        events.insertOne(newEvent)
+            .then(result => {
+                res.send(result.insertedCount > 0);
+            })
+    });
+
     // perform actions on the collection object
     //client.close();
 });
